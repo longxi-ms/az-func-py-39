a22a6caccd3c6018559677390731349f32759bd8
Longxi Yin <107824116+longxi-ms@users.noreply.github.com>
2023-02-07 18:00:13 +0800

    updates

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -1,27 +1,21 @@
-import logging
+import sys, logging
 import requests
 
 import azure.functions as func
 from azure.cosmosdb.table.tableservice import TableService
 from azure.cosmosdb.table.models import Entity
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, BlobType
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
+    str_response = ""
+    for p in sys.path:
+        str_response += p
+        str_response += '\n'
+    str_response += '\n\n'
+    str_response += str(BlobServiceClient)
+    str_response += '\n\n'
     logging.info('Python HTTP trigger function processed a request.')
     logging.warning(str(requests))
+    logging.warning(str(BlobServiceClient))
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    return func.HttpResponse(str_response, status_code=200)
